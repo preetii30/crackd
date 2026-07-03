a44318b9a10505fa3e987dba749df558a9391aa6
preetii30 <patidarpriti686@gmail.com>
2026-07-03 12:26:38 +0530

    increase profile size

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,8 +19,8 @@ app.use(cors({
     origin: FRONTEND_URL,
     credentials: true,
 }));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
